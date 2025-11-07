f7dc6973bb7edd8a26aa1201eaa029120bb27389
Víctor Falcón <victoor89@gmail.com>
2025-11-07 19:24:42 +0000

    Update settings profile page

--- routes/settings.php
+++ routes/settings.php
@@ -15,7 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::get('settings/password', [PasswordController::class, 'edit'])->name('user-password.edit');
 
     Route::put('settings/password', [PasswordController::class, 'update'])
