2eaad3204c189830fad05eb92fc4679fa817065b
Víctor Falcón <victoor89@gmail.com>
2025-11-07 12:01:58 +0000

    Install Pest

--- tests/Feature/Auth/VerificationNotificationTest.php
+++ tests/Feature/Auth/VerificationNotificationTest.php
@@ -1,44 +1,33 @@
 <?php
 
-namespace Tests\Feature\Auth;
-
 use App\Models\User;
 use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
-use Tests\TestCase;
 
-class VerificationNotificationTest extends TestCase
-{
-    use RefreshDatabase;
+test('sends verification notification', function () {
+    Notification::fake();
 
-    public function test_sends_verification_notification(): void
-    {
-        Notification::fake();
+    $user = User::factory()->create([
+        'email_verified_at' => null,
+    ]);
 
-        $user = User::factory()->create([
-            'email_verified_at' => null,
-        ]);
+    $this->actingAs($user)
+        ->post(route('verification.send'))
+        ->assertRedirect(route('home'));
 
-        $this->actingAs($user)
-            ->post(route('verification.send'))
-            ->assertRedirect(route('home'));
+    Notification::assertSentTo($user, VerifyEmail::class);
+});
 
-        Notification::assertSentTo($user, VerifyEmail::class);
-    }
+test('does not send verification notification if email is verified', function () {
+    Notification::fake();
 
-    public function test_does_not_send_verification_notification_if_email_is_verified(): void
-    {
-        Notification::fake();
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
 
-        $user = User::factory()->create([
-            'email_verified_at' => now(),
-        ]);
+    $this->actingAs($user)
+        ->post(route('verification.send'))
+        ->assertRedirect(route('dashboard', absolute: false));
 
-        $this->actingAs($user)
-            ->post(route('verification.send'))
-            ->assertRedirect(route('dashboard', absolute: false));
-
-        Notification::assertNothingSent();
-    }
-}
+    Notification::assertNothingSent();
+});
--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -1,24 +1,13 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class DashboardTest extends TestCase
-{
-    use RefreshDatabase;
+test('guests are redirected to the login page', function () {
+    $this->get(route('dashboard'))->assertRedirect(route('login'));
+});
 
-    public function test_guests_are_redirected_to_the_login_page()
-    {
-        $this->get(route('dashboard'))->assertRedirect(route('login'));
-    }
+test('authenticated users can visit the dashboard', function () {
+    $this->actingAs($user = User::factory()->create());
 
-    public function test_authenticated_users_can_visit_the_dashboard()
-    {
-        $this->actingAs($user = User::factory()->create());
-
-        $this->get(route('dashboard'))->assertOk();
-    }
-}
+    $this->get(route('dashboard'))->assertOk();
+});
