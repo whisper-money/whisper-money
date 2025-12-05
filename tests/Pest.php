d800ebc4e5c297d3fae9a5b01668deba1a6cd51a
Víctor Falcón <victoor89@gmail.com>
2025-12-05 16:00:46 +0100

    Increase browser test timeout from 10s to 30s

--- tests/Pest.php
+++ tests/Pest.php
@@ -19,7 +19,7 @@ pest()->extend(Tests\TestCase::class)
     ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Browser');
 
-pest()->browser()->timeout(10000);
+pest()->browser()->timeout(30000);
 
 /*
 |--------------------------------------------------------------------------
