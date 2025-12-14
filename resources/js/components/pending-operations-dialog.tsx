21f1501bc2fd58bb3439265cc40b37a8110a5906
Víctor Falcón <victoor89@gmail.com>
2025-12-14 12:44:29 +0100

    Max width in mobile view

--- resources/js/components/pending-operations-dialog.tsx
+++ resources/js/components/pending-operations-dialog.tsx
@@ -67,7 +67,7 @@ export function PendingOperationsDialog({
     const canSync = isOnline && !isSyncing;
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-            <DialogContent className="mx-4 max-w-2xl sm:mx-0">
+            <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Pending Operations</DialogTitle>
                     <DialogDescription>
