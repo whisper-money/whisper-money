51bcafa600749029376fe2af570d6d8c6100fe4c
Víctor Falcón <victoor89@gmail.com>
2025-12-15 09:23:59 +0100

    hide amount when key is not set

--- resources/js/components/ui/amount-display.tsx
+++ resources/js/components/ui/amount-display.tsx
@@ -1,3 +1,4 @@
+import { useEncryptionKey } from '@/contexts/encryption-key-context';
 import { usePrivacyMode } from '@/contexts/privacy-mode-context';
 import { cn } from '@/lib/utils';
 import { useEffect, useMemo, useState } from 'react';
@@ -20,24 +21,22 @@ export function AmountDisplay({
     maximumFractionDigits = 2,
 }: AmountDisplayProps) {
     const { isPrivacyModeEnabled } = usePrivacyMode();
+    const { isKeySet } = useEncryptionKey();
     const [amount, setAmount] = useState<number>(amountInCents / 100);
 
+    const shouldHideAmount = isPrivacyModeEnabled || !isKeySet
+
     useEffect(() => {
-        if (isPrivacyModeEnabled) {
-            // return a random amount with the same lenfth as amountInCents
-            const length = amountInCents.toString().length;
+        if (shouldHideAmount) {
+            const length = Math.min(3, amountInCents.toString().length);
+            const fakeAmountStr = parseInt("8".repeat(length - 2) + "00");
 
-            let randomAmountInCents = '';
-            for (let i = 0; i < length; i++) {
-                randomAmountInCents += Math.floor(Math.random() * 10).toString();
-            }
-
-            setAmount(parseInt(randomAmountInCents, 10) / 100)
+            setAmount(fakeAmountStr / 100);
             return;
         }
 
         setAmount(amountInCents / 100);
-    }, [amountInCents, isPrivacyModeEnabled]);
+    }, [amountInCents, shouldHideAmount]);
 
     const formatted = useMemo(() => {
         return new Intl.NumberFormat('en-US', {
@@ -49,7 +48,7 @@ export function AmountDisplay({
     }, [amount, currencyCode, minimumFractionDigits, maximumFractionDigits]);
 
     return (
-        <span className={cn('transition-all duration-300', { 'blur-sm': isPrivacyModeEnabled }, className)}>
+        <span className={cn('transition-all duration-300', { 'rounded-xs bg-foreground': shouldHideAmount }, className)}>
             {showSign && amount >= 0 && '+'}
             {formatted}
         </span>
