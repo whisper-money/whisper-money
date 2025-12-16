d6ec9ff290c618e4eb3b301ef89cbe98c6a3dd44
Víctor Falcón <victoor89@gmail.com>
2025-12-16 13:39:47 +0100

    new onboarding funnel id

--- resources/js/lib/constants.ts
+++ resources/js/lib/constants.ts
@@ -1,3 +1,3 @@
 export const LEAD_FUNNEL_EVENT_UUID = '9668a06c-dee9-47a8-9bee-eaaa2a3a5915';
 export const IMPORT_FUNNEL_EVENT_UUID = 'eea25dce-75ef-4959-b975-a7e9094b3ef5';
-export const ONBOARDING_FUNNEL_EVENT_UUID = '1478ba44-c5c4-4398-83a1-814b21cd7e34';
+export const ONBOARDING_FUNNEL_EVENT_UUID = '8a10c1da-080b-44cc-8dcf-78734b29106c';
