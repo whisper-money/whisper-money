d5d3c1ef1b00964a83f93534a9cb54b69d7305c4
Víctor Falcón <victoor89@gmail.com>
2025-12-17 17:02:05 +0100

    horizon: takes snapshot every 5 min

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
