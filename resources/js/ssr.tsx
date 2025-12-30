99e2c5faa756f924c941f39ed00d5a49464f23de
Víctor Falcón <victoor89@gmail.com>
2025-12-30 08:44:44 +0100

    SSR was missing the privacy mode provider

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -3,7 +3,9 @@ import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
 import { EncryptionKeyProvider } from './contexts/encryption-key-context';
+import { PrivacyModeProvider } from './contexts/privacy-mode-context';
 import { SyncProvider } from './contexts/sync-context';
+import type { SharedData } from './types';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -18,11 +20,22 @@ createServer((page) =>
                 import.meta.glob('./pages/**/*.tsx'),
             ),
         setup: ({ App, props }) => {
+            const initialPageProps = props.initialPage?.props as
+                | Partial<SharedData>
+                | undefined;
+            const initialUser = initialPageProps?.auth?.user ?? null;
+            const initialIsAuthenticated = Boolean(initialUser);
+
             return (
                 <EncryptionKeyProvider>
-                    <SyncProvider initialIsAuthenticated={false}>
-                        <App {...props} />
-                    </SyncProvider>
+                    <PrivacyModeProvider>
+                        <SyncProvider
+                            initialIsAuthenticated={initialIsAuthenticated}
+                            initialUser={initialUser}
+                        >
+                            <App {...props} />
+                        </SyncProvider>
+                    </PrivacyModeProvider>
                 </EncryptionKeyProvider>
             );
         },
