f3ce3ca1645f468ed77328c1823ebcc00f1b0e76
Víctor Falcón <victoor89@gmail.com>
2026-02-24 20:59:08 +0100

    fix: add missing Request import in Settings AccountController

--- app/Http/Controllers/Settings/AccountController.php
+++ app/Http/Controllers/Settings/AccountController.php
@@ -9,6 +9,7 @@ use App\Models\Account;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
