e18b6955a9a789c51d862e2386668c738fac147e
Víctor Falcón <victoor89@gmail.com>
2026-03-16 14:25:48 +0100

    chore: remove unused LEAD_REDIRECT_URL env var

--- config/landing.php
+++ config/landing.php
@@ -14,16 +14,4 @@ return [
 
     'hide_auth_buttons' => env('HIDE_AUTH_BUTTONS', false),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Lead Redirect URL
-    |--------------------------------------------------------------------------
-    |
-    | The external URL to redirect users to after they submit their email
-    | on the landing page.
-    |
-    */
-
-    'lead_redirect_url' => env('LEAD_REDIRECT_URL', null),
-
 ];
