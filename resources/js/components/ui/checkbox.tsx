25beedb6e3f6da3911eccd1694762b4e8943e91a
Víctor Falcón <victoor89@gmail.com>
2026-06-03 16:55:09 +0200

    Update checkbox UI

--- resources/js/components/ui/checkbox.tsx
+++ resources/js/components/ui/checkbox.tsx
@@ -12,7 +12,7 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer group border-input data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground data-[state=checked]:border-primary data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-primary-foreground data-[state=indeterminate]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer group border-input data-[state=checked]:bg-primary/5 dark:data-[state=checked]:bg-primary/20 data-[state=checked]:text-primary data-[state=checked]:border-primary/25 data-[state=indeterminate]:bg-primary/5 dark:data-[state=indeterminate]:bg-primary/20 data-[state=indeterminate]:text-primary-foreground data-[state=indeterminate]:border-primary/25 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
@@ -21,8 +21,8 @@ function Checkbox({
         data-slot="checkbox-indicator"
         className="flex items-center justify-center text-current transition-none"
       >
-        <CheckIcon className="size-3.5 group-data-[state=indeterminate]:hidden" />
-        <MinusIcon className="hidden size-3.5 group-data-[state=indeterminate]:block" />
+        <CheckIcon className="size-3.5 stroke-primary group-data-[state=indeterminate]:hidden" />
+        <MinusIcon className="hidden stroke-primary size-3.5 group-data-[state=indeterminate]:block" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   )
